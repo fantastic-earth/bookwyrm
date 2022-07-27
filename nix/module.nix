863332738c66fdae5f8294fe2c7d0fb4ce3fee5f
D Anzorge <d.anzorge@gmail.com>
2022-07-27 21:58:46 +0200

    nix: unpin nonflake nixpkgs

--- nix/module.nix
+++ nix/module.nix
@@ -1,14 +1,2 @@
 {pkgs, ...}@args:
-  import ./moduleInner.nix (args // (
-    let 
-      pinnedPkgs = import (pkgs.fetchFromGitHub {
-          owner = "NixOS";
-          repo = "nixpkgs";
-          rev = "914ef51ffa88d9b386c71bdc88bffc5273c08ada";
-          sha256 = "sha256-A+oT+aQGhW5lXy8H0cqBLsYtgcnT5glmGOXWQDcGw6I=";
-        }) { 
-          # we need to inherit config to propagate allowUnfree settings (which
-          # are needed for bookwyrm)
-          inherit (pkgs) system config; 
-        };
-    in { bookwyrm = pinnedPkgs.callPackage ./default.nix { };}))
+  import ./moduleInner.nix (args // ({ bookwyrm = pkgs.callPackage ./default.nix { };}))
